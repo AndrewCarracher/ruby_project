66b8d065242cfb7208e9ae1994290a8c5f75a168
AndrewCarracher <andrew.carracher@hotmail.co.uk>
2018-10-05 15:03:38 +0100

    restaurant and covers classes initialized

--- models/cover.rb
+++ models/cover.rb
@@ -1,10 +1,13 @@
 class Cover
 
-  attr_accessor
+  attr_accessor :space_available, :time
+  attr_reader :id, :customer_id
 
-  def initialize
-
-
+  def initialize(options)
+    @id = options['id'].to_i() if options['id']
+    @customer_id = options['customer_id'].to_i()
+    @space_available = options['space_available']
+    @time = options['time']
   end
 
 end
--- models/restaurant.rb
+++ models/restaurant.rb
@@ -1,10 +1,10 @@
 class Restaurant
 
-  attr_accessor
+  attr_accessor :opening_hours
 
-  def initialize
-
-
+  def initialize(options)
+    @id = options['id'].to_i() if options['id']
+    @opening_hours = options['opening_hours']
   end
 
 end
