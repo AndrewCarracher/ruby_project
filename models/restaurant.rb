63aa0ba1569584129b686e1cd989b4ab1b2b8116
AndrewCarracher <andrew.carracher@hotmail.co.uk>
2018-10-05 16:51:57 +0100

    add customer to cover and remove methods added to restaurant class

--- models/restaurant.rb
+++ models/restaurant.rb
@@ -1,3 +1,5 @@
+require_relative( 'cover.rb' )
+
 class Restaurant
 
   attr_accessor :opening_hours
@@ -8,4 +10,25 @@ class Restaurant
     @opening_hours = options['opening_hours']
   end
 
+  def add_customer_to_table(customer_id)
+    sql = " INSERT INTO covers
+    (
+      customer_id
+      )
+      VALUES
+      (
+        $1
+        )
+        RETURNING id"
+    values = [@customer_id]
+    results = SqlRunner.run(sql, values)
+    @id = results.first()['id'].to_i
+  end
+
+  def remove_customer_from_clan(customer_id)
+    "DELETE FROM covers
+    WHERE customer_id = $1"
+    results = SqlRunner.run( sql, values)
+    return Cover.new( results.first )
+  end
 end
