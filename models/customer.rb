a89aacbeb1b9b25284f77e8b08507c84860e3c08
AndrewCarracher <andrew.carracher@hotmail.co.uk>
2018-10-05 14:58:40 +0100

    class customer initialized

--- models/customer.rb
+++ models/customer.rb
@@ -1,13 +1,14 @@
 class Customer
 
-  attr_accessor
+  attr_accessor :first_name, :last_name, :email, :phone_number
+  attr_reader :id
 
   def initialize(options)
-    @id
-    @first_name
-    @last_name
-    @email
-    @phone_number
+    @id = options['id'].to_i if options['id']
+    @first_name = options['first_name']
+    @last_name = options['last_name']
+    @email = options['email']
+    @phone_number = options['phone_number']
   end
 
 end
